1a3bd1934c83eb980c7f6e3bcaacce19e60f9ba3
BluJay04 <arunpakalomattom20@gmail.com>
2025-11-14 15:54:17 +0530

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+export default function Navbar() {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backdropFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {[
+            { title: "Cocktails", id: "cocktails" },
+            { title: "About Us", id: "about" },
+            { title: "The Art", id: "work" },
+            { title: "Contact", id: "contact" },
+          ].map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
